97e34e3dcba47d8a02223f6210fd3f2eb4d9abc5
Jonathan Eduardo de Oliveira <jonathan.mega2@gmail.com>
2024-09-10 20:17:25 -0300

    classe

--- jonathan.java
+++ jonathan.java
@@ -1,1 +1,6 @@
-ECHO est  ativado.
+public class Jonathan {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Obrigado meus amigos pela ajuda!");
+    }
+}
